713c4c90ec7f1ecf6be112c616ad573a90a471a2
Victor Hugo Targino <127980648+vhtargino@users.noreply.github.com>
2023-09-20 16:41:47 -0300

    Add files via upload

---
+++ Aula5 - Sistema Acadêmico/src/Aluno.java
@@ -0,0 +1,35 @@
+public class Aluno extends Pessoa{
+    private double nota1;
+    private double nota2;
+    private double media;
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public double calcularMedia(double nota1, double nota2) {
+        media = (nota1 + nota2)/2;
+        return media;
+
+    }
+}
---
+++ Aula5 - Sistema Acadêmico/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc1 = new Scanner(System.in).useLocale(Locale.US);  // Para strings
+        Scanner sc2 = new Scanner(System.in).useLocale(Locale.US);  // Para números
+
+        String escolha;
+
+        do {
+            System.out.print("Cadastrar professor (1) ou aluno (2): ");
+            escolha = sc2.nextLine();
+
+            switch (escolha) {
+                case "1":
+                    System.out.println("CADASTRO DE PROFESSOR");
+                    Professor prof1 = new Professor();
+                    System.out.print("Digite seu nome: ");
+                    prof1.setNome(sc1.nextLine());
+                    System.out.print("Digite sua idade: ");
+                    prof1.setIdade(sc2.nextInt());
+                    System.out.print("Digite sua especialização: ");
+                    prof1.setEspecializacao(sc1.nextLine());
+                    System.out.print("Digite seu salário: ");
+                    prof1.setSalario(sc2.nextDouble());
+                    break;
+                case "2":
+                    System.out.println("CADASTRO DE ALUNO");
+                    Aluno aluno1 = new Aluno();
+                    System.out.print("Digite seu nome: ");
+                    aluno1.setNome(sc1.nextLine());
+                    System.out.print("Digite sua idade: ");
+                    aluno1.setIdade(sc2.nextInt());
+                    System.out.print("Digite sua primeira nota: ");
+                    aluno1.setNota1(sc2.nextDouble());
+                    System.out.print("Digite sua segunda nota: ");
+                    aluno1.setNota2(sc2.nextDouble());
+                    aluno1.calcularMedia(aluno1.getNota1(), aluno1.getNota2());
+                    System.out.println("Média: " + aluno1.getMedia());
+                    break;
+                default:
+                    System.out.println("Inválido. Escolha 1 ou 2.");
+                    break;
+            }
+        } while(escolha != "1" && escolha != "2");  // CORRIGIR!
+    }
+}
